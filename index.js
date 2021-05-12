92264549d6c63ad1827bc2fd50ce81a0bc8c4ab8
alannanoguchi <alanna.noguchi@gmail.com>
2021-05-11 17:59:12 -0700

    created

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node 
+
+console.log(`
+\nAlanna Noguchi\n
+Website: http://uhhllama.us/
+LinkedIn: https://www.linkedin.com/in/alanna-noguchi-827022192/
+Github: https://github.com/alannanoguchi
+`);
